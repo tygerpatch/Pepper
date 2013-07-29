7e9dd68d78ad93e5a28e8de0605d70b9851c9717
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-07-29 11:36:28 -0400

    Added tests for step method models.

---
+++ tests/org/pepper/core/models/MyClass.java
@@ -0,0 +1,80 @@
+package org.pepper.core.models;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.pepper.core.StepDefinition;
+import org.pepper.core.annotations.Given;
+import org.pepper.core.annotations.Then;
+import org.pepper.core.annotations.When;
+
+// This class demonstrates how to parameterize steps.
+public class MyClass extends StepDefinition {
+
+  private int x;
+  private boolean flag;
+  private double y;
+
+  // *** GIVENS
+  @Given("I have $value")
+  public void given(int value) {
+    x = value;
+  }
+
+  @Given("I have $value")
+  public void givenDbl(double value) {
+    y = value;
+  }
+
+  // Notice that although the above two step methods have the same text,
+  // Pepper calls the correct one based on the given argument types.
+
+  @Given("the flag is $value")
+  public void givenTheFlagIsFalse(boolean value) {
+    flag = value;
+  }
+
+  private String firstName;
+
+  @Given("first name is $something")
+  public void givenTheFirstNameIs(String firstName) {
+    this.firstName = firstName;
+  }
+
+  // *** WHENS
+  @When("I add $value")
+  public void when(int value) {
+    x = x + value;
+  }
+
+  @When("I add $dbl")
+  public void whenDbl(double value) {
+    y = y + value;
+  }
+  // Again, notice that Pepper calls the correct step method based on the given argument types.
+
+  @When("I check the flag")
+  public void whenICheckTheFlag() {
+  }
+
+  // *** THENS
+  @Then("I should have $value")
+  public void then(int value) {
+    assertThat(x, equalTo(value));
+  }
+
+  @Then("I should have $value")
+  public void thenDbl(double value) {
+    assertThat(y, equalTo(value));
+  }
+
+  @Then("it should be $value")
+  public void thenItShouldBeFalse(boolean value) {
+    assertThat(flag, equalTo(value));
+  }
+
+  @Then("I should say $greeting")
+  public void thenIShouldSay(String str) {
+    assertThat("Hello " + firstName, equalTo(str));
+  }
+}
---
+++ tests/org/pepper/core/models/TestStepMethod.java
@@ -0,0 +1,69 @@
+package org.pepper.core.models;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.pepper.core.annotations.Given;
+import org.pepper.core.models.GivenFrameworkMethod;
+import org.pepper.core.models.StepMethod;
+
+// TODO: need a better name for class (depending on what happens to StepMethod class)
+public class TestStepMethod {
+
+  @Test
+  public void testMatches() {
+    String line = "Given I have -3";
+    Class<?> klass = MyClass.class;
+    GivenFrameworkMethod givenMethod = null;
+
+    for (Method method : klass.getMethods()) {
+      if (method.getAnnotation(Given.class) != null) {
+        givenMethod = new GivenFrameworkMethod(method);
+        if (givenMethod.matches(line)) {
+          break;
+        }
+      }
+    }
+
+    assertNotNull(givenMethod);
+    assertThat(givenMethod.getStep(), equalTo("Given I have $value"));
+
+    // TODO: research why a super class can be down casted to subclass?
+    // ex. GivenFrameworkMethod givenMethod = (GivenFrameworkMethod) frameworkMethod;
+  }
+
+  @Test
+  public void testParseArgument() {
+    Object obj;
+
+    // ensure integer arguments get parsed as an integer
+    obj = StepMethod.parseArgument("123");
+    assertTrue("java.lang.Integer".equals(obj.getClass().getName()));
+
+    // ensure that numbers with a decimal get parsed as a double
+    obj = StepMethod.parseArgument("1.23");
+    assertTrue("java.lang.Double".equals(obj.getClass().getName()));
+
+    // ensure that "true" get parsed as a boolean
+    obj = StepMethod.parseArgument("true");
+    assertTrue("java.lang.Boolean".equals(obj.getClass().getName()));
+
+    // ensure that "false" get parsed as a boolean
+    obj = StepMethod.parseArgument("false");
+    assertTrue("java.lang.Boolean".equals(obj.getClass().getName()));
+
+    // ensure that parsing a boolean is case insensitive
+    obj = StepMethod.parseArgument("tRuE");
+    assertTrue("java.lang.Boolean".equals(obj.getClass().getName()));
+
+    // ensure that parsing a boolean is case insensitive
+    obj = StepMethod.parseArgument("fAlSe");
+    assertTrue("java.lang.Boolean".equals(obj.getClass().getName()));
+  }
+
+}
